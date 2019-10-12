2e5bde45919bb96d3d9709d7f4b9e68478879632
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2019-10-12 14:19:52 -0400

    more graphql;

--- userGraphQl/schema/schema.js
+++ userGraphQl/schema/schema.js
@@ -1,33 +1,48 @@
 const graphql = require('graphql');
 const fetch = require('node-fetch');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+} = graphql;
 
 const CompanyType = new GraphQLObjectType({
     name: 'company',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         description: { type: GraphQLString },
-    },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, _) {
+                return fetch(`http://localhost:3000/companies/${parentValue.id}/users`).then(res =>
+                    res.json()
+                );
+            },
+        },
+    }),
 });
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLID },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
-        companyId: { type: GraphQLID },
         company: {
             type: CompanyType,
             resolve(parentValue, _) {
+                console.log(parentValue);
                 return fetch(`http://localhost:3000/companies/${parentValue.companyId}`).then(res =>
                     res.json()
                 );
             },
         },
-    },
+    }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -35,14 +50,14 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         user: {
             type: UserType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(_, args) {
                 return fetch(`http://localhost:3000/users/${args.id}`).then(resp => resp.json());
             },
         },
         company: {
             type: CompanyType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(_, args) {
                 return fetch(`http://localhost:3000/companies/${args.id}`).then(resp =>
                     resp.json()
